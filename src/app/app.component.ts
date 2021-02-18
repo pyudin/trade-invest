1143034372edc0cd91d0d3f7b40d927f4f7eefd0
Pavel Yudin <pasha.judin@gmail.com>
2021-02-18 23:52:01 +0200

    basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'trade-invest';
+export class AppComponent implements OnInit {
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  isSmallScreen: Observable<any> = this.breakpointObserver
+    .observe(['(max-width: 599px)'])
+    .pipe(map((res) => res.matches));
+
+  ngOnInit() {
+    this.isSmallScreen.subscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { LayoutModule } from '@angular/cdk/layout';
+import { MainComponent } from './main/main.component';
+import { StartedComponent } from './started/started.component';
+import { PeopleComponent } from './people/people.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavbarComponent, FooterComponent, MainComponent, StartedComponent, PeopleComponent, QuestionsComponent, ContactComponent],
+  imports: [BrowserModule, LayoutModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
