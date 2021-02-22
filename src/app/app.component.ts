31068fb1614d08fdcd11dc64b24ead842c59e76f
Pavel Yudin <pasha.judin@gmail.com>
2021-02-22 09:39:05 +0200

    last fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+// import { Observable } from 'rxjs';
+// import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +11,11 @@ import { map } from 'rxjs/operators';
 export class AppComponent implements OnInit {
   constructor(private breakpointObserver: BreakpointObserver) {}
 
-  isSmallScreen: Observable<any> = this.breakpointObserver
-    .observe(['(max-width: 599px)'])
-    .pipe(map((res) => res.matches));
+  // isSmallScreen: Observable<any> = this.breakpointObserver
+  //   .observe(['(max-width: 599px)'])
+  //   .pipe(map((res) => res.matches));
 
   ngOnInit() {
-    this.isSmallScreen.subscribe();
+    // this.isSmallScreen.subscribe();
   }
 }
